8510fe3251dac83f32b0851e493e284be00b4b0c
Mike Schutte <tmikeschutte@gmail.com>
2023-11-06 21:40:16 -0500

    queue passing

--- queues/lib/queue.rb
+++ queues/lib/queue.rb
@@ -1,3 +1,47 @@
 class Queue
+  attr_reader :count
 
+  def initialize
+    @head = Node.new(:head)
+    @tail = Node.new(:tail, @head)
+    @count = 0
+  end
+
+  def enqueue(x)
+    @count += 1
+    node = Node.new(x)
+    node.right = @tail
+    left = @tail.left
+    left.right = node
+    @tail.left = node
+  end
+
+  def dequeue
+    @count -= 1
+    popped = @head.right
+    @head.right = popped.right
+    popped.val
+  end
+
+  def peek
+    @head.right.val
+  end
+
+  def last
+    @tail.left.val
+  end
+
+  def empty
+    @count == 0
+  end
+end
+
+class Node
+  attr_accessor :val, :left, :right
+
+  def initialize(val, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
 end
--- queues/test/queue_test.rb
+++ queues/test/queue_test.rb
@@ -1,6 +1,6 @@
-require 'minitest'
-require 'minitest/autorun'
-require './lib/queue.rb'
+require "minitest"
+require "minitest/autorun"
+require "./lib/queue.rb"
 
 class StackTest < Minitest::Test
   def test_it_enqueues
@@ -21,4 +21,32 @@ class StackTest < Minitest::Test
     assert_equal("C", queue.dequeue)
   end
 
+  def test_it_peeks
+    queue = Queue.new
+    queue.enqueue("A")
+    queue.enqueue("B")
+    queue.enqueue("C")
+    assert_equal("A", queue.peek)
+    queue.dequeue
+    assert_equal("B", queue.peek)
+  end
+
+  def test_it_gets_last
+    queue = Queue.new
+    queue.enqueue("A")
+    queue.enqueue("B")
+    queue.enqueue("C")
+    assert_equal("C", queue.last)
+    queue.dequeue
+    assert_equal("C", queue.last)
+  end
+
+  def test_it_gets_empty
+    queue = Queue.new
+    assert_equal(true, queue.empty)
+    queue.enqueue("A")
+    assert_equal(false, queue.empty)
+    queue.dequeue
+    assert_equal(true, queue.empty)
+  end
 end
